e5812a8d1b87e915516e316408a8e5317c242153
CanutoIS <canutosorianoivan@gmail.com>
2024-02-12 11:04:15 +0100

    add auth controllesrs and routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.routes.js'
+
 const app = express()
 dotenv.config()
 
@@ -9,5 +12,19 @@ app.get('/', (req, res) => {
     // root toute http://localhost:5000/
     res.send('Hello World')
 })
+
+app.use('/api/auth', authRoutes)
+
+// app.get('api/auth/signup', (req, res) => {
+//     console.log('signup route')
+// })
+
+// app.get('api/auth/login', (req, res) => {
+//     console.log('login route')
+// })
+
+// app.get('api/auth/logout', (req, res) => {
+//     console.log('logout route')
+// })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
